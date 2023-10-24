a3f80bc8c19c96e8ccae797d650d00976e35f0ad
thamindumk <thamindu.20@cse.mrt.ac.lk>
2023-10-24 19:53:34 +0530

    load relevant profile to parent

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import {
+  viewChildProfilesForParentsAsync,
   getChildProfilesAsync,
   getStaffProfileListAsync,
   getSocialWorkerProfileListAsync,
@@ -45,7 +46,7 @@ import {
   createApprovalLogAsync,
   getProfileCountForOrphanageAsync,
   getStaffCountForOrphanageAsync,
-  getParentCountForOrphanageAsync
+  getParentCountForOrphanageAsync,
 } from "../services/profileService.js";
 
 import { RPCRequest } from "../lib/rabbitmq/index.js";
@@ -360,15 +361,18 @@ export const createParentProfile = asyncHandler(async (req, res) => {
  * Delete Profiles
  */
 export const deleteChildProfile = asyncHandler(async (req, res) => {
-
   const { childId, commitMessage, committedByUserName } = JSON.parse(
     req.body.otherInfo
   );
   const profileData = await getChildProfileAllDetailsAsync(childId);
   const committedByUserId = await getUserByEmailAsync(committedByUserName);
-  const State="DELETED";
-  const ApprovalLogId=await createApprovalLogAsync(State, ReviewedBy, req.userInfo.userId); // reviewed by null value
-  await childProfileDeleteRequestAsync(ApprovalLogId,childId,commitMessage);
+  const State = "DELETED";
+  const ApprovalLogId = await createApprovalLogAsync(
+    State,
+    ReviewedBy,
+    req.userInfo.userId
+  ); // reviewed by null value
+  await childProfileDeleteRequestAsync(ApprovalLogId, childId, commitMessage);
   if (profileData) {
     await CreateProfileVersionAsync(
       childId,
@@ -785,9 +789,9 @@ export const getProfileVersion = asyncHandler(async (req, res) => {
   });
 });
 export const createInquiry = asyncHandler(async (req, res) => {
-  const{Subject,Description}= req.body;
-  const CreatedBy=req.userInfo.userId;
-  await createInquiryAsync(CreatedBy,Subject,Description);
+  const { Subject, Description } = req.body;
+  const CreatedBy = req.userInfo.userId;
+  await createInquiryAsync(CreatedBy, Subject, Description);
 
   return res.status(200).json({
     success: true,
@@ -796,9 +800,9 @@ export const createInquiry = asyncHandler(async (req, res) => {
 });
 
 export const childProfileDeleteRequest = asyncHandler(async (req, res) => {
-  const{ChildId,Remark}= req.body;
-  const ApprovalId=req.userInfo.userId;
-  await childProfileDeleteRequestAsync(ApprovalId,ChildId,Remark);
+  const { ChildId, Remark } = req.body;
+  const ApprovalId = req.userInfo.userId;
+  await childProfileDeleteRequestAsync(ApprovalId, ChildId, Remark);
 
   return res.status(200).json({
     success: true,
@@ -807,10 +811,21 @@ export const childProfileDeleteRequest = asyncHandler(async (req, res) => {
 });
 
 export const createFund = asyncHandler(async (req, res) => {
-  const{Name, Email, Mobile, TransactionAmount, Description}= req.body;
-  const State="CREATED";
-  const ApprovalLogId=await createApprovalLogAsync(State, ReviewedBy, req.userInfo.userId); // reviewed by null value
-  await createFundAsync(Name, Email, Mobile, TransactionAmount, ApprovalLogId, Description);
+  const { Name, Email, Mobile, TransactionAmount, Description } = req.body;
+  const State = "CREATED";
+  const ApprovalLogId = await createApprovalLogAsync(
+    State,
+    ReviewedBy,
+    req.userInfo.userId
+  ); // reviewed by null value
+  await createFundAsync(
+    Name,
+    Email,
+    Mobile,
+    TransactionAmount,
+    ApprovalLogId,
+    Description
+  );
 
   return res.status(200).json({
     success: true,
@@ -836,9 +851,19 @@ export const getStaffCountForOrphanage = asyncHandler(async (req, res) => {
 });
 
 export const getParentCountForOrphanage = asyncHandler(async (req, res) => {
-  const result = await getParentCountForOrphanageAsync(req.userInfo.orphanageId);
+  const result = await getParentCountForOrphanageAsync(
+    req.userInfo.orphanageId
+  );
   return res.status(200).json({
     success: true,
     count: result[0].count,
+  });
+});
+
+export const viewChildProfilesForParents = asyncHandler(async (req, res) => {
+  const result = await viewChildProfilesForParentsAsync(req.userInfo.userId);
+  return res.status(200).json({
+    success: true,
+    profile: result,
   });
 });
